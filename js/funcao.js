0465dd16a7d0c65bf76c861b0d4551e1bdb96d0a
Maycon Ramalho Pereira <mayconramalhopereira@outlook.com.br>
2018-11-23 18:50:55 -0200

    atualizações

--- js/funcao.js
+++ js/funcao.js
@@ -12,16 +12,19 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var Altura = tdAltura.textContent;
 
+// query selector traz algum elemento
 var tdImc = paciente.querySelector(".info-imc");
 var pesoValido = true;
 var alturaValida = true;
+
+
 if(peso < 0 || peso > 1000){
     alert("Peso Invalido");
     pesoValido = false;
 }
 
-if(Altura < 0 || Altura > 2,5){
-    alert("Peso Invalido");
+if(Altura < 0 || Altura > 2.50){
+    alert("Altura Invalida!");
     alturaValida = false;    
 }
 
