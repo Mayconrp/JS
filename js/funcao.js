876d1677aa5b14cb703715e037e4164e4dfe623c
Maycon Ramalho Pereira <mayconramalhopereira@outlook.com.br>
2018-11-24 19:32:00 -0200

    Adicionando dados de um form a lista de dados na tela

--- js/funcao.js
+++ js/funcao.js
@@ -49,7 +49,47 @@ for(var i = 0 ; i < pacientes.length ; i++){
 //titulo.addEventListener("click", function() {
 //   console.log(" olha função anônima! ");
 
-titulo.addEventListener("click", mostraMensagem);
-    function mostraMensagem(){
-    console.log("Olá eu fui clicado!");
-}
+    var botaoAdicionar =  document.querySelector("#adicionar-paciente");
+    botaoAdicionar.addEventListener("click", function (event) {
+        // evitar comportamentos padrões como enviar um form atualizando a página
+        event.preventDefault();
+        
+        var formulario = document.querySelector("#formAdiciona");
+        var nome = formulario.nome.value;  // observar as ordens
+        var peso = formulario.peso.value;
+        var altura = formulario.altura.value;
+        var gordura = formulario.gordura.value;
+
+        var pacienteTr = document.createElement("tr");
+
+        var nomeTd = document.createElement("td");
+        var pesoTd = document.createElement("td");
+        var alturaTd = document.createElement("td");
+        var gorduraTd = document.createElement("td");
+        var imcTd = document.createElement("td");
+
+        nomeTd.textContent = nome;
+        pesoTd.textContent = peso;
+        alturaTd.textContent = altura;
+        gorduraTd.textContent = gordura;
+
+        pacienteTr.appendChild(nomeTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(gorduraTd);
+
+        var tabela = document.querySelector("#tabela-pacientes");
+
+        tabela.appendChild(pacienteTr);
+
+});
+
+
+
+
+
+
+
+
+
+
